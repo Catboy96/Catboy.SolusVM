16dd2c57499fabfd0c43ba225aa2905d87d45d7f
Ralf Ren <me@ralf.ren>
2019-04-06 20:33:33 +0800

    Add files via upload

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 有关程序集的一般信息由以下
+' 控制。更改这些特性值可修改
+' 与程序集关联的信息。
+
+'查看程序集特性的值
+
+<Assembly: AssemblyTitle("SolusVM API Library")>
+<Assembly: AssemblyDescription("SolusVM API Library")>
+<Assembly: AssemblyCompany("Ralf Ren")>
+<Assembly: AssemblyProduct("Catboy.SolusVM")>
+<Assembly: AssemblyCopyright("©2019 Ralf Ren. All rights reserved.")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+<Assembly: Guid("6f25f506-cb7d-4dad-abbf-785d483ba794")>
+
+' 程序集的版本信息由下列四个值组成: 
+'
+'      主版本
+'      次版本
+'      生成号
+'      修订号
+'
+' 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
+' 方法是按如下所示使用“*”: :
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ SolusVMClient.vb
@@ -0,0 +1,102 @@
+﻿Imports System.Threading.Tasks
+Imports System.Net
+Imports System.Text.RegularExpressions
+
+Namespace SolusVM
+    Public Class SolusVMClient
+
+        Public Property ApiKey As String
+        Public Property ApiHash As String
+        Public Property Endpoint As String
+
+        Public Structure ClientReturnStructure
+            Public status As String
+            Public statusmsg As String
+            Public hostname As String
+            Public vmstat As String
+            Public ipaddr As String
+            Public ipaddress As String
+            Public hdd As String
+            Public mem As String
+            Public bw As String
+        End Structure
+
+        Public Sub New()
+            ApiKey = ""
+            ApiHash = ""
+            Endpoint = ""
+        End Sub
+
+        Public Sub New(ApiKey As String, ApiHash As String, Endpoint As String)
+            Me.ApiKey = ApiKey
+            Me.ApiHash = ApiHash
+            Me.Endpoint = Endpoint
+        End Sub
+
+        Public Async Function BootAsync() As Task(Of ClientReturnStructure)
+            Dim query_string As String = $"{Endpoint}/api/client/command.php?key={ApiKey}&hash={ApiHash}&action=boot"
+            Dim return_string As String = Await (New WebClient).DownloadStringTaskAsync(New Uri(query_string))
+            Return New ClientReturnStructure With {
+                .status = Regex.Match(return_string, "(?<=\<status\>).*(?=\</status\>)").Value,
+                .statusmsg = Regex.Match(return_string, "(?<=\<statusmsg\>).*(?=\</statusmsg\>)").Value,
+                .hostname = Regex.Match(return_string, "(?<=\<hostname\>).*(?=\</hostname\>)").Value,
+                .ipaddress = Regex.Match(return_string, "(?<=\<ipaddress\>).*(?=\</ipaddress\>)").Value,
+                .vmstat = Regex.Match(return_string, "(?<=\<vmstat\>).*(?=\</vmstat\>)").Value
+            }
+        End Function
+
+        Public Async Function RebootAsync() As Task(Of ClientReturnStructure)
+            Dim query_string As String = $"{Endpoint}/api/client/command.php?key={ApiKey}&hash={ApiHash}&action=reboot"
+            Dim return_string As String = Await (New WebClient).DownloadStringTaskAsync(New Uri(query_string))
+            Return New ClientReturnStructure With {
+                .status = Regex.Match(return_string, "(?<=\<status\>).*(?=\</status\>)").Value,
+                .statusmsg = Regex.Match(return_string, "(?<=\<statusmsg\>).*(?=\</statusmsg\>)").Value,
+                .hostname = Regex.Match(return_string, "(?<=\<hostname\>).*(?=\</hostname\>)").Value,
+                .ipaddress = Regex.Match(return_string, "(?<=\<ipaddress\>).*(?=\</ipaddress\>)").Value,
+                .vmstat = Regex.Match(return_string, "(?<=\<vmstat\>).*(?=\</vmstat\>)").Value
+            }
+        End Function
+
+        Public Async Function ShutdownAsync() As Task(Of ClientReturnStructure)
+            Dim query_string As String = $"{Endpoint}/api/client/command.php?key={ApiKey}&hash={ApiHash}&action=shutdown"
+            Dim return_string As String = Await (New WebClient).DownloadStringTaskAsync(New Uri(query_string))
+            Return New ClientReturnStructure With {
+                .status = Regex.Match(return_string, "(?<=\<status\>).*(?=\</status\>)").Value,
+                .statusmsg = Regex.Match(return_string, "(?<=\<statusmsg\>).*(?=\</statusmsg\>)").Value,
+                .hostname = Regex.Match(return_string, "(?<=\<hostname\>).*(?=\</hostname\>)").Value,
+                .ipaddress = Regex.Match(return_string, "(?<=\<ipaddress\>).*(?=\</ipaddress\>)").Value,
+                .vmstat = Regex.Match(return_string, "(?<=\<vmstat\>).*(?=\</vmstat\>)").Value
+            }
+        End Function
+
+        Public Async Function StatusAsync() As Task(Of ClientReturnStructure)
+            Dim query_string As String = $"{Endpoint}/api/client/command.php?key={ApiKey}&hash={ApiHash}&action=status"
+            Dim return_string As String = Await (New WebClient).DownloadStringTaskAsync(New Uri(query_string))
+            Return New ClientReturnStructure With {
+                .status = Regex.Match(return_string, "(?<=\<status\>).*(?=\</status\>)").Value,
+                .statusmsg = Regex.Match(return_string, "(?<=\<statusmsg\>).*(?=\</statusmsg\>)").Value,
+                .hostname = Regex.Match(return_string, "(?<=\<hostname\>).*(?=\</hostname\>)").Value,
+                .ipaddress = Regex.Match(return_string, "(?<=\<ipaddress\>).*(?=\</ipaddress\>)").Value,
+                .vmstat = Regex.Match(return_string, "(?<=\<vmstat\>).*(?=\</vmstat\>)").Value
+            }
+        End Function
+
+        Public Async Function InfoAsync() As Task(Of ClientReturnStructure)
+            Dim quer_string As String = $"{Endpoint}/api/client/command.php?key={ApiKey}&hash={ApiHash}&action=info" &
+                                        "&bw=true&hdd=true&mem=true&ipaddr=true"
+            Dim return_string As String = Await (New WebClient).DownloadStringTaskAsync(New Uri(quer_string))
+            Return New ClientReturnStructure With {
+                .status = Regex.Match(return_string, "(?<=\<status\>).*(?=\</status\>)").Value,
+                .statusmsg = Regex.Match(return_string, "(?<=\<statusmsg\>).*(?=\</statusmsg\>)").Value,
+                .hostname = Regex.Match(return_string, "(?<=\<hostname\>).*(?=\</hostname\>)").Value,
+                .vmstat = Regex.Match(return_string, "(?<=\<vmstat\>).*(?=\</vmstat\>)").Value,
+                .ipaddr = Regex.Match(return_string, "(?<=\<ipaddr\>).*(?=\</ipaddr\>)").Value,
+                .ipaddress = Regex.Match(return_string, "(?<=\<ipaddress\>).*(?=\</ipaddress\>)").Value,
+                .hdd = Regex.Match(return_string, "(?<=\<hdd\>).*(?=\</hdd\>)").Value,
+                .mem = Regex.Match(return_string, "(?<=\<mem\>).*(?=\</mem\>)").Value,
+                .bw = Regex.Match(return_string, "(?<=\<bw\>).*(?=\</bw\>)").Value
+            }
+        End Function
+
+    End Class
+End Namespace
